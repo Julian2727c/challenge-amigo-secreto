619c4d82b8e0bce21ad70eba19f451f4fe94b5fb
Julian2727c <juliancardenas2727@gmail.com>
2025-08-05 20:41:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+console.log("¡Bienvenido al desafío de lógica de programación!");
+// Aquí puedes comenzar a escribir tu código para resolver el desafío.
